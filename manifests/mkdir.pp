beff6e0e3264f39a8b06cafe87bddcd41841632b
Vladislav Nazarenko <vladislav.nazarenko@dvag.com>
2014-03-19 09:17:05 +0100

    mkdir now only ensures the directory is in the catalog, the were conflicts, when the directory was already created by another manifests

--- manifests/mkdir.pp
+++ manifests/mkdir.pp
@@ -1,17 +1,16 @@
 # Nasty ass hax to allow several levels of directories
+# Ensures the directory exists in the catalag
 define nfs::mkdir() {
+    if ! defined (File[$name]) {
+        exec { "mkdir_recurse_${name}" :
+            path    => [ '/bin', '/usr/bin' ],
+            command => "mkdir -p ${name}",
+            unless  => "test -d ${name}",
+        }
 
-  exec { "mkdir_recurse_${name}":
-    path    => [ '/bin', '/usr/bin' ],
-    command => "mkdir -p ${name}",
-    unless  => "test -d ${name}",
-  }
-
-  file {
-    "${name}":
-      ensure  => directory,
-      require => Exec["mkdir_recurse_${name}"],
-      mode => "0777",
-  }
-
+        file { $name :
+            ensure  => directory,
+            require => Exec["mkdir_recurse_${name}"]
+        }
+    }
 }
